c5a75089d99435d23ad08f3360c5f5ef37ada42b
amngupta <amangupta96@outlook.com>
2017-01-16 18:38:18 -0800

    Fix to represent how the code for each promise will look like

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,29 +1,41 @@
 /**
  * This is the main programmatic entry point for the project.
  */
-import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
+import { IInsightFacade, InsightResponse, QueryRequest } from "./IInsightFacade";
 import Helpers from './memberFunctions'
 
 import Log from "../Util";
 
 export default class InsightFacade implements IInsightFacade {
-    
-    private helpers:Helpers = new Helpers();
+
+    private helpers: Helpers = null;
 
     constructor() {
+        this.helpers = new Helpers();
         Log.trace('InsightFacadeImpl::init()');
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        this.helpers.uncompressFile('courses.zip');
-        return null;
+        return new Promise((fulfill, reject) => {
+            this.helpers.uncompressFile('courses.zip')
+                .then((response) => {
+
+                })
+                .catch((err) => {
+                    reject({
+                        code: 400,
+                        body: "Test Failed"
+                    })
+                });
+        });
+
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
         return null;
     }
 
-    performQuery(query: QueryRequest): Promise <InsightResponse> {
+    performQuery(query: QueryRequest): Promise<InsightResponse> {
         return null;
     }
 }
