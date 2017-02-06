e93977a7d93df1e877e8353a3bd535fec2d739da
amngupta <amangupta96@outlook.com>
2017-02-06 02:44:07 -0800

    Added start and stop test for improved cover on rest

--- test/EchoSpec.ts
+++ test/EchoSpec.ts
@@ -3,12 +3,12 @@
  */
 
 import Server from "../src/rest/Server";
-import {expect} from 'chai';
+import { expect } from 'chai';
 import Log from "../src/Util";
-import {InsightResponse} from "../src/controller/IInsightFacade";
+import { InsightResponse } from "../src/controller/IInsightFacade";
 
 describe("EchoSpec", function () {
-
+    let ServerS: Server = null;
 
     function sanityCheck(response: InsightResponse) {
         expect(response).to.have.property('code');
@@ -17,6 +17,7 @@ describe("EchoSpec", function () {
     }
 
     before(function () {
+        ServerS = new Server(3000);
         Log.test('Before: ' + (<any>this).test.parent.title);
     });
 
@@ -32,12 +33,33 @@ describe("EchoSpec", function () {
         Log.test('AfterTest: ' + (<any>this).currentTest.title);
     });
 
+    it("Should be able to start server", function () {
+        return ServerS.start()
+            .then((res) => {
+                expect(res).to.equal(true);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    });
+
+
+    it("Should be able to stop server", function () {
+        return ServerS.stop()
+            .then((res) => {
+                expect(res).to.equal(true);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    });
+
     it("Should be able to echo", function () {
         let out = Server.performEcho('echo');
         Log.test(JSON.stringify(out));
         sanityCheck(out);
         expect(out.code).to.equal(200);
-        expect(out.body).to.deep.equal({message: 'echo...echo'});
+        expect(out.body).to.deep.equal({ message: 'echo...echo' });
     });
 
     it("Should be able to echo silence", function () {
@@ -45,7 +67,7 @@ describe("EchoSpec", function () {
         Log.test(JSON.stringify(out));
         sanityCheck(out);
         expect(out.code).to.equal(200);
-        expect(out.body).to.deep.equal({message: '...'});
+        expect(out.body).to.deep.equal({ message: '...' });
     });
 
     it("Should be able to handle a missing echo message sensibly", function () {
@@ -53,7 +75,7 @@ describe("EchoSpec", function () {
         Log.test(JSON.stringify(out));
         sanityCheck(out);
         expect(out.code).to.equal(400);
-        expect(out.body).to.deep.equal({error: 'Message not provided'});
+        expect(out.body).to.deep.equal({ error: 'Message not provided' });
     });
 
     it("Should be able to handle a null echo message sensibly", function () {
@@ -62,7 +84,7 @@ describe("EchoSpec", function () {
         sanityCheck(out);
         expect(out.code).to.equal(400);
         expect(out.body).to.have.property('error');
-        expect(out.body).to.deep.equal({error: 'Message not provided'});
+        expect(out.body).to.deep.equal({ error: 'Message not provided' });
     });
 
 });
