844d6c3068c515c016801a0039268183882671f1
amngupta <amangupta96@outlook.com>
2017-03-29 12:31:46 -0700

    Adding index.js

---
+++ src/rest/client/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+// import { Router } from 'react-router';
+// import App from './App';
+import Routes from './routes';
+import './index.css';
+
+ReactDOM.render(
+  Routes,
+  document.getElementById('root')
+);
